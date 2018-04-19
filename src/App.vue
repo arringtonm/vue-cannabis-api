92e3e91a432a5900fb9098579d0d9a9e8505e540
Michael Arrington <michaelarringtoniphone@gmail.com>
2018-04-19 15:38:20 -0700

    Add tabs for search type

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,54 @@
 <template>
   <div id="app">
-    <h1>
+    <h1>Grooooovy
     </h1>
-    <form id="strain" @submit.prevent="searchStrain">
-      <div v-for="strain in strains">
-        <input type="radio" name="strain" :id="strain" :value="strain" v-model="strainChecked">{{ strain }}<br>
-      </div>
-      <button type="submit">Search</button>
-    </form>
-    <!-- <form id="strain-effects">
-      <div v-for="effect in effects">
-        <input type="radio" name="effect" :id="effect" :value="effect">{{ effect }}<br>
-      </div>
-    </form> -->
-
-    <ul id="strainOutput">
-      <li v-for="strain in strainResponse" :key="strain.id">
-        {{ strain.name }}
-      </li>
+    <ul>
+      <li id="searchTabsName" class="searchNav" v-bind:class="{ active : searchTab == 'name'}" @click="searchTab = 'name'">Name</li>
+      <li id="searchTabsRace" class="searchNav" v-bind:class="{ active : searchTab == 'race'}" @click="searchTab = 'race'">Race</li>
+      <li id="searchTabsEffects" class="searchNav" v-bind:class="{ active : searchTab == 'effects'}" @click="searchTab = 'effects'">Effects</li>
     </ul>
-  </div>
+    <div id="searchBox">
+
+
+
+<!-- name tab -->
+
+      <div v-if="searchTab == 'name'">
+        <form id="name" @submit.prevent="searchName">
+          <input type="text" placeholder="Strain name or keyword" v-model="searchByName">
+          <button type="submit">Search</button>
+        </form>
+
+
+      </div>
+
+<!-- race tab  -->
+      <div v-if="searchTab == 'race'">
+        <form id="strain" @submit.prevent="searchRace">
+          <div v-for="strain in strains">
+            <input type="radio" name="strain" :id="strain" :value="strain" v-model="raceChecked">{{ strain }}<br>
+          </div>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+<!-- effects tab  -->
+      <div v-if="searchTab == 'effects'">
+        <form id="strain-effects">
+          <div v-for="effect in effects">
+            <input type="radio" name="effect" :id="effect" :value="effect">{{ effect }}<br>
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+      <ul id="strainOutput">
+        <li v-for="strain in strainResponse" :key="strain.id" class="strainname" @click="searchDetails(strain.id)">
+          {{ strain.name }}
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script>
@@ -30,40 +59,105 @@ export default {
   name: 'app',
   data () {
     return {
+      searchTab: 'race',
       strainChecked: '',
+      searchByName: '',
+      strainId: '',
+      strainIdResponse: '',
       strainResponse: [],
       strainApiKey: strainApiKey,
       msg: 'Let\'s refactor this!',
-      strains: ['indica', 'sativa', 'hybrid'],
+      strains: ['Indica', 'Sativa', 'Hybrid'],
       effects: ['anxious', 'aroused', 'cramps', 'creative']
     }
   },
   methods: {
-    searchStrain: function() {
-      // alert(this.strainChecked)
-      axios.get(`http://strainapi.evanbusse.com/${strainApiKey}/strains/search/race/${this.strainChecked}`).then(response => {
+    searchRace: function() {
+      axios.get(`http://strainapi.evanbusse.com/${strainApiKey}/strains/search/race/${this.raceChecked}`).then(response => {
         this.strainResponse = response.data
       })
-
-
+    },
+    searchDetails: function(strain) {
+      axios.get(`http://strainapi.evanbusse.com/${strainApiKey}/strains/data/desc/${strain}`).then(response => {
+        this.strainIdResponse = response.data
+      })
+      // console.log(strain);
     }
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #F3F3F1;
+}
+
 #app {
-  font-family: sans-serif;
+  padding: 100px;
+  font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   // text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #444;
+  font-weight: 200;
+  // margin-top: 60px;
+}
+
+.strainname {
+  text-decoration: underline;
+  text-decoration-color: green;
+  &:hover {
+    cursor: pointer
+  }
+}
+
+#searchBox {
+  margin-top: -6px;
+  border: 1px solid #83468c;
+  background-color: white;
+  padding: 25px;
+}
+
+li.searchNav {
+  // width: 70px;
+  display: inline-block;
+  // justify-content: center;
+ // border: 1px solid #999;
+  padding: 10px;
+  // border-bottom: 5px solid #F3F3F1;
+  // border-bottom: 1px solid #83468c;
+  margin-left: -3px;
+  margin-right: -2px;
+  color: #777;
+  margin-bottom: -16px;
+  &:hover {
+    cursor: pointer;
+  }
+
+}
+
+.active {
+  // border-bottom: 5px solid #83468c !important;
+  border: 1px solid #83468c;
+
+  border-bottom: 1px solid white;
+  color: #444 !important;
+  background-color: white;
+
 }
 
 h1, h2 {
-  font-weight: normal;
+  font-weight: 900;
 }
+
+
+// purple: #83468c
+// yellow: #F7E042
+// beige: #F3F3F1
 
 // ul {
 //   list-style-type: none;
